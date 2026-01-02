30cbc86d0d879add4fda4a3595cac59f0778094a
emonahmed123 <imonshomon@gmail.com>
2026-01-02 11:37:42 +0600

    steup project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import './App.css'
-import Counter from './components/Counter'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Navbar from './components/Navbar'
+import TodoList from './components/TodoList'
 
 function App() {
 
   return (
-    <div className='mx-auto max-w-[1170px] h-svh'>
-      <Counter />
+    <div className='grid place-items-center bg-blue-100 min-h-screen px-6'>
+      <Navbar />
+
+      <div className='w-full max-w-3xl shadow-lg p-6 bg-white'>
+        <Header />
+
+        <hr className='mt-4' />
+        <TodoList />
+        <hr className='mt-4' />
+        <Footer />
+
+      </div>
     </div >
   )
 }
